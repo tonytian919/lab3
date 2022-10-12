b0c09f65640b2c586d414214d5d29ab1d246f657
Yaotian <yawu@ucsd.edu>
2022-10-12 15:36:00 -0700

    lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,25 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverseInPlace3() {
+    int[] input1 = { 1, 2, 3 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 3, 2, 1 }, input1);
+	}
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = { 1, 2 };
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{ 2, 1 }, input1);
+	}
+
+  @Test
+  public void testReversed1() {
+    int[] input1 = { 1, 2, 3};
+    assertArrayEquals(new int[]{ 3, 2, 1 }, ArrayExamples.reversed(input1));
+  }
+
 }
