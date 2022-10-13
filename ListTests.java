3a111852402d6e233e73e42e836ef16b14f89763
Yaotian <yawu@ucsd.edu>
2022-10-13 01:45:26 -0700

    Update ListTests.java

--- ListTests.java
+++ ListTests.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ListTests {
     @Test
-    public void test
+    public void testFilter() {
+        List<String> lstring = new ArrayList( );
+        List<String> lstring1 = new ArrayList( );
+        lstring.filter(new checkString());
+        assertArrayEquals( lstring1, ListExamples.filter( lstring, checkString(String s) ));
+    }
 }
