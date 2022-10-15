fb9520a4a97f739e246b51d7f2887b740170b23e
Yaotian <yawu@ucsd.edu>
2022-10-14 18:27:48 -0700

    listcheck

--- ListTests.java
+++ ListTests.java
@@ -3,13 +3,14 @@ import java.util.List;
 import static org.junit.Assert.*;
 import org.junit.*;
 
+// given a substring, checkString checks if the parameter contains the substring.
 class someChecker implements StringChecker {
     String str;
-    public someChecker() {
-        this.str = str;
+    public someChecker(String s) {
+        this.str = s;
     }
     public boolean checkString(String s) {
-        return this.str.equals(s);
+        return s.contains(this.str);
     }
 }
 
@@ -17,21 +18,36 @@ public class ListTests {
 
     @Test
     public void testFilter() {
-        List<String> lstring = new ArrayList<>( );
-        List<String> lstring1 = new ArrayList<>( );
-        StringChecker sc = new someChecker();
-        lstring1 = ListExamples.filter(lstring, sc);
-        assertEquals(lstring, lstring1);
+        List<String> input = new ArrayList<>( );
+        input.add("head");
+        input.add("hello");
+        input.add("water");
+        input.add("bean");
+        input.add("yellow");
+        input.add("mean");
+        input.add("lean");
+        input.add("memo");
+        input.add("leak");
+        List<String> expected = new ArrayList<>( );
+        expected.add("head");
+        expected.add("bean");
+        expected.add("mean");
+        expected.add("lean");
+        expected.add("leak");
+        List<String> output = new ArrayList<>( );
+        StringChecker sc = new someChecker("ea");
+        output = ListExamples.filter(input, sc);
+        assertEquals(expected, output);
     }
 
-    @Test
-    public void testFilter2() {
-        List<String> lstring = new ArrayList<>();
-        List<String> lstring1 = new ArrayList<>();
-        lstring.add("apple");
-        lstring.add("banana");
-        StringChecker sc = new someChecker();
-        lstring1 = ListExamples.filter(lstring, sc);
-        assertEquals(lstring, lstring1);
-    }
+    // @Test
+    // public void testFilter2() {
+    //     List<String> lstring = new ArrayList<>();
+    //     List<String> lstring1 = new ArrayList<>();
+    //     lstring.add("apple");
+    //     lstring.add("banana");
+    //     StringChecker sc = new someChecker();
+    //     lstring1 = ListExamples.filter(lstring, sc);
+    //     assertEquals(lstring, lstring1);
+    // }
 }
